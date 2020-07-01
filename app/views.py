063021595217944d0d165368c88a4090f5ed2e6d
Wörner, Maximilian <maximilian.woerner@accenture.com>
2020-07-01 10:48:10 +0200

    Bug fixes and count limitations

--- app/views.py
+++ app/views.py
@@ -20,33 +20,45 @@ def index():
     
     if request.method == 'POST':
         subnetsInput=request.form['subnetsInput']
-        #removeLastOctet=request.form['removeLastOctet']
-        #removeLastOctet=request.POST.get('removeLastOctet')
 
-        if form.removeLastOctet.data:
-            ipList = OrderedSet()
-        else:
-            ipList = list()
+        #if form.removeLastOctet.data:
+        ipList = OrderedSet()
+        #else:
+        #    ipList = list()
 
         if subnetsInput is not None:
             subnetList = subnetsInput.split()
 
+            ipCount = 0
+            subnetCount = 0
+
             try:
+
+                # Check count of IPs within the given networks
                 for subnet in subnetList:
-                    for ip in ipaddress.IPv4Network(subnet):
+                    ipCount = ipCount + ipaddress.IPv4Network(subnet).num_addresses
+                    subnetCount = subnetCount + 1
 
-                        if form.removeLastOctet.data:
+                print(subnetCount)
 
+                for subnet in subnetList:
+
+                    if form.removeLastOctet.data and ipCount/256 <= 10000:
+                        for ip in ipaddress.IPv4Network(subnet):
                             # Remove last octet of IP
                             ip3 = separator.join(str(ip).split(separator, 3)[:-1])
-
                             # Add IP to list if not duplicate
                             ipList.add(ip3)
 
-                        else:
-
+                    elif ipCount <= 10000:
+                        for ip in ipaddress.IPv4Network(subnet):
+                        
                             # Return 4 octets
-                            ipList.append(ip)
+                            #ipList.append(ip)
+                            ipList.add(ip)
+                    else:
+                        flash('The count of IPs resulting from your input would be too high (max. 10.000). Consider removing the last octet by clicking on the checkbox below to shorten your output.')
+                        break
 
             except ValueError as e:
                 flash('Please check the subnet format. All subnets must use the CIDR notation (e.g. 192.168.178.0/24).')
